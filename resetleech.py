ccd2af0d59d1bb4038d3e430d83c22fa5be9301b
Jake Mays <quaunaut@gmail.com>
2011-11-02 14:39:21 -0700

    Cleaning.

--- resetleech.py
+++ resetleech.py
@@ -2,16 +2,17 @@
 Created on Oct 31, 2011
 
 @author: quaunaut
+
+Adds a button to reset the 'Leech' status to a card.
 '''
 
-# Adds a button to reset the 'Leech' status to a card.
+
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from ankiqt import mw
 from anki.hooks import addHook
 from anki.cards import Card
-#from anki.utils import hasTag, delTag
     
 
 def setupMenu(editor):
@@ -24,8 +25,6 @@ def setupMenu(editor):
     
 
 def resetLeech(editor):
-    n = "Reset Leech b"
-    # if mw.currentCard.hasTag("leech")
     # Reps(known as "Reviews" inside of Anki) is set
     # to 1 instead of 0 because of a generic
     # database error.
@@ -35,4 +34,4 @@ def resetLeech(editor):
 
 addHook("editor.setupMenus", setupMenu)
     
-mw.registerPlugin("Reset Leech!", 18)
+mw.registerPlugin("Reset Leech", 18)
